71b56f646854dc9c919618773318ed92b847e458
Sander van Vliet <sander@codenizer.nl>
2022-01-27 21:45:00 +0100

    Clean up handle message use case

--- src/RoadCaptain/UseCases/HandleZwiftMessagesUseCase.cs
+++ src/RoadCaptain/UseCases/HandleZwiftMessagesUseCase.cs
@@ -23,37 +23,54 @@ namespace RoadCaptain.UseCases
             while (!token.IsCancellationRequested)
             {
                 // Dequeue will block if there are no messages in the queue
-                var message  = _emitter.Dequeue(token);
+                var message = _emitter.Dequeue(token);
 
                 if (message is ZwiftRiderPositionMessage riderPosition)
                 {
-                    _monitoringEvents.RiderPositionReceived(riderPosition.Latitude, riderPosition.Longitude);
+                    HandleRiderPositionMessage(riderPosition);
                 }
                 else if (message is ZwiftPingMessage ping)
                 {
-                    if (!_pingedBefore)
-                    {
-                        lock (SyncRoot)
-                        {
-                            if (_pingedBefore)
-                            {
-                                return;
-                            }
-
-                            _pingedBefore = true;
-                        }
-
-                        _messageReceiver.SendInitialPairingMessage(ping.RiderId);
-                    }
+                    HandlePingMessage(ping);
                 }
                 else if (message is ZwiftCommandAvailableMessage commandAvailable)
                 {
-                        _monitoringEvents.CommandAvailable(commandAvailable.Type);
+                    _monitoringEvents.CommandAvailable(commandAvailable.Type);
                 }
                 else if (message is ZwiftPowerUpMessage powerUp)
                 {
                     _monitoringEvents.PowerUpAvailable(powerUp.Type);
                 }
+            }
+        }
+
+        private void HandleRiderPositionMessage(ZwiftRiderPositionMessage riderPosition)
+        {
+            _monitoringEvents.RiderPositionReceived(riderPosition.Latitude, riderPosition.Longitude);
+
+            /*
+             * Next steps:
+             * - Determine which segment we're on based on the position
+             * - Determine direction on that segment (to which end of the segment are we moving)
+             * - Determine next turn action (left/straight/right)
+             */
+        }
+
+        private void HandlePingMessage(ZwiftPingMessage ping)
+        {
+            if (!_pingedBefore)
+            {
+                lock (SyncRoot)
+                {
+                    if (_pingedBefore)
+                    {
+                        return;
+                    }
+
+                    _pingedBefore = true;
+                }
+
+                _messageReceiver.SendInitialPairingMessage(ping.RiderId);
             }
         }
     }
