0d01b180461ff614a42261e3c58c11e5928b1b58
Sander van Vliet <sander@codenizer.nl>
2022-02-09 10:58:44 +0100

    Return upcoming segment info directly from the sequence

--- src/RoadCaptain/PlannedRoute.cs
+++ src/RoadCaptain/PlannedRoute.cs
@@ -4,11 +4,15 @@ namespace RoadCaptain
 {
     public class PlannedRoute
     {
-        public string StartingSegmentId { get; private set; } = "empty-segment";
-        public string NextSegmentId { get; private set; } = "next-segment";
+        private readonly int _segmentIndex = 0;
+
+        public string StartingSegmentId => RouteSegmentSequence[_segmentIndex].SegmentId;
+        public string NextSegmentId => RouteSegmentSequence[_segmentIndex].NextSegmentId;
+        public List<TurnDirection> TurnsToNextSegment => RouteSegmentSequence[_segmentIndex].ExpectedTurns;
+        public TurnDirection TurnToNextSegment => RouteSegmentSequence[_segmentIndex].TurnToNextSegment;
         public string CurrentSegment { get; private set; }
-        public List<TurnDirection> TurnsToNextSegment { get; set; } = new();
-        public TurnDirection TurnToNextSegment { get; set; } = TurnDirection.GoStraight;
+
+        public List<SegmentSequence> RouteSegmentSequence { get; private set; }
 
         public void EnteredSegment(string segmentId)
         {
@@ -17,5 +21,41 @@ namespace RoadCaptain
                 CurrentSegment = segmentId;
             }
         }
+
+        public static PlannedRoute FixedForTesting()
+        {
+            var route = new PlannedRoute
+            {
+                RouteSegmentSequence = new List<SegmentSequence>
+                {
+                    new()
+                    {
+                        SegmentId = "watopia-bambino-fondo-004-before-after",
+                        NextSegmentId = "watopia-bambino-fondo-004-after-after",
+                        TurnToNextSegment = TurnDirection.GoStraight
+                    },
+                    new()
+                    {
+                        SegmentId = "watopia-bambino-fondo-004-after-after",
+                        NextSegmentId = "watopia-bambino-fondo-004-before-after"
+                    },
+                    new()
+                    {
+                        SegmentId = "watopia-bambino-fondo-004-before-after",
+                        NextSegmentId = "watopia-bambino-fondo-004-after-after"
+                    }
+                }
+            };
+
+            return route;
+        }
+    }
+
+    public class SegmentSequence
+    {
+        public string SegmentId { get; set; }
+        public List<TurnDirection> ExpectedTurns { get; set; }
+        public TurnDirection TurnToNextSegment { get; set; }
+        public string NextSegmentId { get; set; }
     }
 }
