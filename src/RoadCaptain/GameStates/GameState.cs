040cf029845ceb9875602d273fbbc0ba103f9b10
Sander van Vliet <sander@codenizer.nl>
2022-02-22 18:30:47 +0100

    Organise GameState methods

--- src/RoadCaptain/GameStates/GameState.cs
+++ src/RoadCaptain/GameStates/GameState.cs
@@ -4,9 +4,9 @@ namespace RoadCaptain.GameStates
 {
     public abstract class GameState
     {
-        public abstract GameState UpdatePosition(TrackPoint position, List<Segment> segments, PlannedRoute plannedRoute);
         public abstract GameState EnterGame(ulong activityId);
         public abstract GameState LeaveGame();
+        public abstract GameState UpdatePosition(TrackPoint position, List<Segment> segments, PlannedRoute plannedRoute);
         public abstract GameState TurnCommandAvailable(string type);
     }
 }
