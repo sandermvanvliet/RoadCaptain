16b454242e21656cd58766618b94a0b032052fc3
Sander van Vliet <sander@codenizer.nl>
2022-02-24 13:49:31 +0100

    No longer calculate distance between coordinates if they are more than 0.00013 degrees of longitude apart

--- src/RoadCaptain/TrackPoint.cs
+++ src/RoadCaptain/TrackPoint.cs
@@ -41,6 +41,16 @@ namespace RoadCaptain
 
         public bool IsCloseTo(TrackPoint point)
         {
+            // 0.00013 degrees equivalent to 15 meters between degrees at latitude -11 
+            // That means that if the difference in longitude between
+            // the two points is more than 0.00013 then we're definitely
+            // going to be more than 15 meters apart and that means
+            // we're not close.
+            if (Math.Abs(Longitude - point.Longitude) > 0.00013)
+            {
+                return false;
+            }
+
             var distance = GetDistanceFromLatLonInMeters(
                 Latitude, 
                 Longitude,
