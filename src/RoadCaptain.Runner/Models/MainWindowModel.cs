9095b80f9769b20b9bd6f4685369c1618a8fb81a
Sander van Vliet <sander@codenizer.nl>
2022-03-10 09:26:49 +0100

    Add model for main view

--- src/RoadCaptain.Runner/Models/MainWindowModel.cs
+++ src/RoadCaptain.Runner/Models/MainWindowModel.cs
@@ -7,6 +7,12 @@ namespace RoadCaptain.Runner.Models
     public class MainWindowModel : INotifyPropertyChanged
     {
         private string _windowTitle;
+        private double _elapsedDistance;
+        private double _elapsedAscent;
+        private double _elapsedDescent;
+        private SegmentSequenceModel _currentSegment;
+        private SegmentSequenceModel _nextSegment;
+        private int _currentSegmentSequenceNumber;
 
         public string WindowTitle
         {
@@ -23,6 +29,74 @@ namespace RoadCaptain.Runner.Models
             }
         }
 
+        public PlannedRoute Route { get; set; }
+
+        public double ElapsedDistance
+        {
+            get => _elapsedDistance;
+            set
+            {
+                if (value.Equals(_elapsedDistance)) return;
+                _elapsedDistance = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double ElapsedAscent
+        {
+            get => _elapsedAscent;
+            set
+            {
+                if (value.Equals(_elapsedAscent)) return;
+                _elapsedAscent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double ElapsedDescent
+        {
+            get => _elapsedDescent;
+            set
+            {
+                if (value.Equals(_elapsedDescent)) return;
+                _elapsedDescent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SegmentSequenceModel CurrentSegment
+        {
+            get => _currentSegment;
+            set
+            {
+                if (Equals(value, _currentSegment)) return;
+                _currentSegment = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SegmentSequenceModel NextSegment
+        {
+            get => _nextSegment;
+            set
+            {
+                if (Equals(value, _nextSegment)) return;
+                _nextSegment = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int CurrentSegmentSequenceNumber
+        {
+            get => _currentSegmentSequenceNumber;
+            set
+            {
+                if (value == _currentSegmentSequenceNumber) return;
+                _currentSegmentSequenceNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
@@ -30,5 +104,9 @@ namespace RoadCaptain.Runner.Models
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+    }
+
+    public class SegmentSequenceModel
+    {
     }
 }
