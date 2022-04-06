b137cd58dcaed8efcc5f2944df65982bf0fd8b6a
Sander van Vliet <sander@codenizer.nl>
2022-04-06 12:44:24 +0200

    Verify navigation is started when user enters game

---
+++ test/RoadCaptain.Runner.Tests.Unit/Engine/WhenInGameStateIsReceived.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using RoadCaptain.GameStates;
+using Serilog.Events;
+using Serilog.Sinks.InMemory.Assertions;
+using Xunit;
+
+namespace RoadCaptain.Runner.Tests.Unit.Engine
+{
+    public class WhenInGameStateIsReceived : EngineTest
+    {
+        [Fact]
+        public void NavigationTaskIsStarted()
+        {
+            GivenInGameStateIsReceived();
+
+            GetTaskByFieldName("_navigationTask")
+                .Should()
+                .NotBeNull();
+        }
+
+        [Fact]
+        public void GivenNavigationTaskAlreadyStarted_ItIsNotStartedAgain()
+        {
+            GivenTaskIsRunning("_navigationTask");
+            var navigationTask = GetTaskByFieldName("_navigationTask");
+
+            GivenInGameStateIsReceived();
+
+            GetTaskByFieldName("_navigationTask")
+                .Should()
+                .Be(navigationTask);
+        }
+
+        [Fact]
+        public void MessageIsLogged()
+        {
+            GivenInGameStateIsReceived();
+
+            InMemorySink
+                .Should()
+                .HaveMessage("User entered the game")
+                .Appearing()
+                .Once()
+                .WithLevel(LogEventLevel.Information);
+        }
+
+        private void GivenInGameStateIsReceived()
+        {
+            ReceiveGameState(new InGameState(1, 2));
+        }
+    }
+}
