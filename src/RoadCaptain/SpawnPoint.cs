17019a35f934579dc03a85236559c185aedb7531
Sander van Vliet <sander@codenizer.nl>
2022-04-18 17:49:54 +0200

    Add sport to spawn points and add running spawn points for Watopia

--- src/RoadCaptain/SpawnPoint.cs
+++ src/RoadCaptain/SpawnPoint.cs
@@ -5,5 +5,6 @@
         public string SegmentId { get; set; }
         public string ZwiftRouteName { get; set; }
         public SegmentDirection Direction { get; set; }
+        public SportType Sport { get; set; }
     }
 }
