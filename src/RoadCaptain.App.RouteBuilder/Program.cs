8fc359f461292772572b1856765dfb2ee4e05840
Sander van Vliet <sander@codenizer.nl>
2022-05-12 17:30:54 +0200

    Make Route Builder work on MacOS from app bundle

--- src/RoadCaptain.App.RouteBuilder/Program.cs
+++ src/RoadCaptain.App.RouteBuilder/Program.cs
@@ -1,7 +1,9 @@
 using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.ReactiveUI;
 using System;
+#if MACOS
+using System.IO;
+#endif
 
 namespace RoadCaptain.App.RouteBuilder
 {
@@ -11,8 +13,20 @@ namespace RoadCaptain.App.RouteBuilder
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            #if MACOS
+            // When launching from an app bundle (.app) the working directory
+            // is set to be / which prevents us from loading resources...
+            if(Environment.CurrentDirectory == "/")
+            {
+                Environment.CurrentDirectory = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+            }
+            #endif
+
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
