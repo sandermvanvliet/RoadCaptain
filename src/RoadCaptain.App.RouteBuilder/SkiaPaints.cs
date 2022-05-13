8b53b44983148efc18309a555f533655589d2963
Sander van Vliet <sander@codenizer.nl>
2022-05-13 17:20:53 +0200

    Set anti-alias on segment paints

--- src/RoadCaptain.App.RouteBuilder/SkiaPaints.cs
+++ src/RoadCaptain.App.RouteBuilder/SkiaPaints.cs
@@ -5,42 +5,42 @@ namespace RoadCaptain.App.RouteBuilder
     public class SkiaPaints
     {
         public static readonly SKPaint SegmentPathPaint = new()
-            { Color = SKColor.Parse("#000000"), Style = SKPaintStyle.Stroke, StrokeWidth = 4 };
+            { Color = SKColor.Parse("#000000"), Style = SKPaintStyle.Stroke, StrokeWidth = 4, IsAntialias = true };
 
         public static readonly SKPaint SelectedSegmentPathPaint = new()
-            { Color = SKColor.Parse("#ffcc00"), Style = SKPaintStyle.Stroke, StrokeWidth = 6 };
+            { Color = SKColor.Parse("#ffcc00"), Style = SKPaintStyle.Stroke, StrokeWidth = 6, IsAntialias = true };
 
         public static readonly SKPaint SegmentHighlightPaint = new()
-            { Color = SKColor.Parse("#4CFF00"), Style = SKPaintStyle.Stroke, StrokeWidth = 6 };
+            { Color = SKColor.Parse("#4CFF00"), Style = SKPaintStyle.Stroke, StrokeWidth = 6, IsAntialias = true };
 
         public static readonly SKPaint SpawnPointSegmentPathPaint = new()
-            { Color = SKColor.Parse("#44dd44"), Style = SKPaintStyle.Stroke, StrokeWidth = 4 };
+            { Color = SKColor.Parse("#44dd44"), Style = SKPaintStyle.Stroke, StrokeWidth = 4, IsAntialias = true };
 
         public static readonly SKPaint RoutePathPaint = new()
-            { Color = SKColor.Parse("#0000ff"), Style = SKPaintStyle.Stroke, StrokeWidth = 4 };
+            { Color = SKColor.Parse("#0000ff"), Style = SKPaintStyle.Stroke, StrokeWidth = 4, IsAntialias = true };
 
         public static readonly SKPaint RiderPositionFillPaint = new()
-            { Color = SKColor.Parse("#FF6141"), Style = SKPaintStyle.Fill };
+            { Color = SKColor.Parse("#FF6141"), Style = SKPaintStyle.Fill, IsAntialias = true };
 
         public static readonly SKPaint CircleMarkerPaint = new()
-            { Color = SKColor.Parse("#ffffff"), Style = SKPaintStyle.StrokeAndFill, StrokeWidth = 2 };
+            { Color = SKColor.Parse("#ffffff"), Style = SKPaintStyle.StrokeAndFill, StrokeWidth = 2, IsAntialias = true };
 
         public static readonly SKPaint StartMarkerFillPaint = new()
-            { Color = SKColor.Parse("#14c817"), Style = SKPaintStyle.Fill };
+            { Color = SKColor.Parse("#14c817"), Style = SKPaintStyle.Fill, IsAntialias = true };
 
         public static readonly SKPaint EndMarkerFillPaint = new()
-            { Color = SKColor.Parse("#ff0000"), Style = SKPaintStyle.Fill };
+            { Color = SKColor.Parse("#ff0000"), Style = SKPaintStyle.Fill, IsAntialias = true };
 
         public static readonly SKPaint MarkerSegmentStartPaint = new()
-            { Color = SKColor.Parse("#ff0000"), Style = SKPaintStyle.Fill };
+            { Color = SKColor.Parse("#ff0000"), Style = SKPaintStyle.Fill, IsAntialias = true };
 
         public static readonly SKPaint MarkerSegmentEndPaint = new()
-            { Color = SKColor.Parse("#14c817"), Style = SKPaintStyle.Fill };
+            { Color = SKColor.Parse("#14c817"), Style = SKPaintStyle.Fill, IsAntialias = true };
 
         public static readonly SKPaint SprintSegmentPaint = new()
-            { Color = SKColor.Parse("#B200FF"), Style = SKPaintStyle.Stroke, StrokeWidth = 4 };
+            { Color = SKColor.Parse("#B200FF"), Style = SKPaintStyle.Stroke, StrokeWidth = 4, IsAntialias = true };
 
         public static readonly SKPaint ClimbSegmentPaint = new()
-            { Color = SKColor.Parse("#FF6A00"), Style = SKPaintStyle.Stroke, StrokeWidth = 4 };
+            { Color = SKColor.Parse("#FF6A00"), Style = SKPaintStyle.Stroke, StrokeWidth = 4, IsAntialias = true };
     }
 }
