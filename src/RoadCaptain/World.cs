746ee32aebc7c1603dfed163c9fc6a9d49971c70
Sander van Vliet <sander@codenizer.nl>
2022-06-12 12:57:50 +0200

    Add the Zwift numerical world id to the list of worlds

--- src/RoadCaptain/World.cs
+++ src/RoadCaptain/World.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-
-namespace RoadCaptain
+﻿namespace RoadCaptain
 {
     public class World
     {
@@ -9,6 +7,7 @@ namespace RoadCaptain
         public string Description { get; set; }
         public SpawnPoint[] SpawnPoints { get; set; }
         public WorldStatus Status { get; set; }
+        public ZwiftWorldId ZwiftId { get; set; } = ZwiftWorldId.Unknown;
     }
 
     public enum WorldStatus
@@ -16,5 +15,21 @@ namespace RoadCaptain
         Unknown,
         Available,
         Unavailable
+    }
+
+    public enum ZwiftWorldId
+    {
+        Unknown = -1,
+        Watopia = 1,
+        Richmond = 2,
+        London = 3,
+        NewYork = 4,
+        Innsbruck = 5,
+        Bologna = 6,
+        Yorkshire = 7,
+        CritCity = 8,
+        MakuriIslands = 9,
+        France = 10,
+        Paris = 11
     }
 }
