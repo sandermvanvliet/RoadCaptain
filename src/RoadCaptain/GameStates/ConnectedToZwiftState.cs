8ab55b681fc9cd9f4621dbf3fb243e247729fc43
Sander van Vliet <sander@codenizer.nl>
2022-08-20 17:07:23 +0200

    Add tests for ConnectedToZwiftState

--- src/RoadCaptain/GameStates/ConnectedToZwiftState.cs
+++ src/RoadCaptain/GameStates/ConnectedToZwiftState.cs
@@ -4,27 +4,27 @@ namespace RoadCaptain.GameStates
 {
     public sealed class ConnectedToZwiftState : GameState
     {
-        public override GameState UpdatePosition(TrackPoint position, List<Segment> segments, PlannedRoute plannedRoute)
-        {
-            return this;
-        }
-
         // TODO: Set RiderId because we should already know this when we're connected to Zwift
-        public sealed override uint RiderId => 0;
+        public override uint RiderId => 0;
 
-        public sealed override GameState EnterGame(uint riderId, ulong activityId)
+        public override GameState EnterGame(uint riderId, ulong activityId)
         {
             return new InGameState(riderId, activityId);
         }
 
         public override GameState LeaveGame()
         {
-            return this;
+            throw InvalidStateTransitionException.NotLoggedIn(GetType());
+        }
+
+        public override GameState UpdatePosition(TrackPoint position, List<Segment> segments, PlannedRoute plannedRoute)
+        {
+            throw InvalidStateTransitionException.NotInGame(GetType());
         }
 
         public override GameState TurnCommandAvailable(string type)
         {
-            return this;
+            throw InvalidStateTransitionException.NotOnARoute(GetType());
         }
     }
 }
---
+++ test/RoadCaptain.Tests.Unit/GameState/FromConnectedToZwiftState.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using RoadCaptain.GameStates;
+using Xunit;
+
+namespace RoadCaptain.Tests.Unit.GameState
+{
+    public class FromConnectedToZwiftState : StateTransitionTestBase
+    {
+        [Fact]
+        public void GivenPositionNotOnSegment_InvalidStateTransitionExceptionIsThrown()
+        {
+            var action = () => GivenStartingState().UpdatePosition(PointNotOnAnySegment, Segments, Route);
+
+            action
+                .Should()
+                .Throw<InvalidStateTransitionException>();
+        }
+
+        [Fact]
+        public void GivenPositionOnSegment_InvalidStateTransitionExceptionIsThrown()
+        {
+            var action = () => GivenStartingState().UpdatePosition(Segment1Point1, Segments, Route);
+
+            action
+                .Should()
+                .Throw<InvalidStateTransitionException>();
+        }
+
+        [Fact]
+        public void EnteringGameWithRiderAndActivityId_ResultIsInGameState()
+        {
+            var result = GivenStartingState().EnterGame(1, 2);
+
+            result
+                .Should()
+                .BeOfType<InGameState>()
+                .Which
+                .RiderId
+                .Should()
+                .Be(1);
+        }
+
+        [Fact]
+        public void LeavingGame_InvalidStateTransitionExceptionIsThrown()
+        {
+            var action = () => GivenStartingState().LeaveGame();
+
+            action
+                .Should()
+                .Throw<InvalidStateTransitionException>();
+        }
+
+        [Fact]
+        public void TurnCommandAvailable_InvalidStateTransitionExceptionIsThrown()
+        {
+            var action = () => GivenStartingState().TurnCommandAvailable("left");
+
+            action
+                .Should()
+                .Throw<InvalidStateTransitionException>();
+        }
+
+        private ConnectedToZwiftState GivenStartingState()
+        {
+            return new ConnectedToZwiftState();
+        }
+    }
+}
