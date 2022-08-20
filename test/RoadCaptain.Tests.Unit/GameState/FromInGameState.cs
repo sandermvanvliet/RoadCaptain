49c3c742b31e9243374f57a710f1e0e223c35516
Sander van Vliet <sander@codenizer.nl>
2022-08-20 11:14:05 +0200

    Tidy up FromInGameState tests

--- test/RoadCaptain.Tests.Unit/GameState/FromInGameState.cs
+++ test/RoadCaptain.Tests.Unit/GameState/FromInGameState.cs
@@ -1,28 +1,16 @@
-﻿using System.Collections.Generic;
-using FluentAssertions;
+﻿using FluentAssertions;
 using RoadCaptain.GameStates;
 using Xunit;
 
 namespace RoadCaptain.Tests.Unit.GameState
 {
-    public class FromInGameState
+    public class FromInGameState : StateTransitionTestBase
     {
-        private readonly List<Segment> _segments;
-        private readonly PlannedRoute _route;
-
-        public FromInGameState()
-        {
-            _segments = new List<Segment>();
-            _route = SegmentSequenceBuilder.TestLoopOne();
-        }
-
         [Fact]
         public void GivenPositionNotOnSegment_ResultIsPositionedState()
         {
-            var position = new TrackPoint(1, 2, 3, ZwiftWorldId.Watopia);
-
             var result = new InGameState(1, 2)
-                .UpdatePosition(position, _segments, _route);
+                .UpdatePosition(PointNotOnAnySegment, Segments, Route);
 
             result
                 .Should()
@@ -30,20 +18,14 @@ namespace RoadCaptain.Tests.Unit.GameState
                 .Which
                 .CurrentPosition
                 .Should()
-                .Be(position);
+                .Be(PointNotOnAnySegment);
         }
 
         [Fact]
         public void GivenPositionOnSegment_ResultIsOnSegmentState()
         {
-            var position = new TrackPoint(1, 2, 3, ZwiftWorldId.Watopia);
-            _segments.Add(new Segment(new List<TrackPoint>{ position})
-            {
-                Id = "segment-1"
-            });
-
             var result = new InGameState(1, 2)
-                .UpdatePosition(position, _segments, _route);
+                .UpdatePosition(Segment1Point1, Segments, Route);
 
             result
                 .Should()
@@ -53,21 +35,15 @@ namespace RoadCaptain.Tests.Unit.GameState
         [Fact]
         public void GivenPositionOnSegment_ResultIsOnSegmentStateForSegmentOne()
         {
-            var position = new TrackPoint(1, 2, 3, ZwiftWorldId.Watopia);
-            _segments.Add(new Segment(new List<TrackPoint>{ position})
-            {
-                Id = "segment-1"
-            });
-
             var result = new InGameState(1, 2)
-                .UpdatePosition(position, _segments, _route);
+                .UpdatePosition(Segment1Point1, Segments, Route);
 
             result
                 .As<OnSegmentState>()
                 .CurrentSegment
                 .Id
                 .Should()
-                .Be("segment-1");
+                .Be(Segment1.Id);
         }
 
         [Fact]
@@ -132,7 +108,7 @@ namespace RoadCaptain.Tests.Unit.GameState
         }
 
         [Fact]
-        public void TurnCommandAvaialble_InvalidStateTransitionExceptionIsThrown()
+        public void TurnCommandAvailable_InvalidStateTransitionExceptionIsThrown()
         {
             var action = () => new InGameState(1, 2).TurnCommandAvailable("left");
 
