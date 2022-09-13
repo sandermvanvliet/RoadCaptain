553294e8d91e000b04965dfbadef6731ba329e92
Sander van Vliet <sander@codenizer.nl>
2022-09-13 15:05:54 +0200

    Fix a nullability indicator

--- src/RoadCaptain/SegmentSequence.cs
+++ src/RoadCaptain/SegmentSequence.cs
@@ -8,7 +8,7 @@ namespace RoadCaptain
     {
         public string SegmentId { get; set; }
         public TurnDirection TurnToNextSegment { get; set; } = TurnDirection.None;
-        public string NextSegmentId { get; set; }
+        public string? NextSegmentId { get; set; }
         public SegmentDirection Direction { get; set; } = SegmentDirection.Unknown;
         public SegmentSequenceType Type { get; set; } = SegmentSequenceType.Unknown;
     }
