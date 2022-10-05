92d18a415e73f0788d80ec88b0d719cf80749315
Sander van Vliet <sander@codenizer.nl>
2022-10-05 15:46:36 +0200

    Add network connection experiment

---
+++ test/RoadCaptain.Adapters.Tests.Unit/Networking/NetworkConnection.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RoadCaptain.Adapters.Tests.Unit.Networking
+{
+    internal class NetworkConnection
+    {
+        private readonly int _port;
+        private Socket? _listeningSocket;
+        private readonly CancellationTokenSource _tokenSource;
+        private readonly TimeSpan _acceptTimeout;
+        private readonly TimeSpan _dataTimeout;
+        private Socket? _clientSocket;
+
+        public NetworkConnection(int port, int acceptTimeout, int dataTimeout)
+        {
+            _port = port;
+            _tokenSource = new CancellationTokenSource();
+            _acceptTimeout = TimeSpan.FromMilliseconds(acceptTimeout);
+            _dataTimeout = TimeSpan.FromMilliseconds(dataTimeout);
+        }
+
+        public event EventHandler? IncomingConnectionWatchdog;
+        public event EventHandler? IncomingDataWatchdog;
+        public event EventHandler? ConnectionLost;
+        public event EventHandler? ConnectionAccepted;
+        public event EventHandler<DataEventArgs>? Data;
+
+        public async Task StartAsync()
+        {
+            _listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+            {
+                NoDelay = true
+            };
+
+            _listeningSocket.Bind(new IPEndPoint(IPAddress.Loopback, _port));
+
+            _listeningSocket.Listen();
+
+            while (!_tokenSource.IsCancellationRequested)
+            {
+                var acceptTask = _listeningSocket.AcceptAsync(_tokenSource.Token).AsTask();
+
+                while (!acceptTask.IsCompleted)
+                {
+                    var timeoutTask = Task.Delay(_acceptTimeout);
+
+                    var completedTask = await Task.WhenAny(timeoutTask, acceptTask);
+
+                    if (completedTask == timeoutTask)
+                    {
+                        IncomingConnectionWatchdog?.Invoke(this, EventArgs.Empty);
+                    }
+                    else
+                    {
+                        // When the network connection is stopped then
+                        // the acceptTask potentially completes before
+                        // the timeout task but it will be in a failed
+                        // state (cancelled).
+                        if (_tokenSource.IsCancellationRequested)
+                        {
+                            return;
+                        }
+
+                        _clientSocket = acceptTask.Result;
+
+                        ConnectionAccepted?.Invoke(this, EventArgs.Empty);
+
+                        break;
+                    }
+                }
+
+                var dataReadTask = Task.Factory.StartNew(ReadDataFromClientSocket);
+
+                while (!_tokenSource.IsCancellationRequested)
+                {
+                    var timeoutTask = Task.Delay(_dataTimeout);
+
+                    var completedTask = await Task.WhenAny(timeoutTask, dataReadTask);
+
+                    if (completedTask == timeoutTask)
+                    {
+                        IncomingDataWatchdog?.Invoke(this, EventArgs.Empty);
+                    }
+                    else
+                    {
+                        // When the network connection is stopped then
+                        // the acceptTask potentially completes before
+                        // the timeout task but it will be in a failed
+                        // state (cancelled).
+                        if (_tokenSource.IsCancellationRequested)
+                        {
+                            return;
+                        }
+
+                        if (dataReadTask.Result is { Length: > 0 })
+                        {
+                            Data?.Invoke(this, new DataEventArgs(dataReadTask.Result));
+                        }
+
+                        if (_clientSocket == null || !_clientSocket.Connected)
+                        {
+                            _clientSocket = null;
+
+                            // Drop back to accepting a new connection
+                            break;
+                        }
+
+                        if (_clientSocket.Connected)
+                        {
+                            dataReadTask = Task.Factory.StartNew(ReadDataFromClientSocket);
+                        }
+                    }
+                }
+            }
+        }
+
+        private byte[]? ReadDataFromClientSocket()
+        {
+            while (!_tokenSource.IsCancellationRequested)
+            {
+                var buffer = new byte[512];
+
+                int read;
+
+                try
+                {
+                    read = _clientSocket!.Receive(buffer, 0, buffer.Length, SocketFlags.None, out var socketError);
+
+                    if (socketError != SocketError.Success || read == 0)
+                    {
+                        Debug.WriteLine("Socket error: " + socketError);
+
+                        CloseAndCleanupClientSocket();
+
+                        return null;
+                    }
+                }
+                catch (SocketException)
+                {
+                    return null;
+                }
+                catch (ObjectDisposedException)
+                {
+                    return null;
+                }
+
+                if (read > 0)
+                {
+                    var result = new byte[read];
+                    
+                    for (var i = 0; i < read; i++)
+                    {
+                        result[i] = buffer[i];
+                    }
+
+                    return result;
+                }
+            }
+
+            return null;
+        }
+
+        private void CloseAndCleanupClientSocket()
+        {
+            try
+            {
+                _clientSocket?.Shutdown(SocketShutdown.Both);
+            }
+            catch (SocketException)
+            {
+                // Don't care
+            }
+            catch (ObjectDisposedException)
+            {
+                _clientSocket = null;
+            }
+
+            try
+            {
+                _clientSocket?.Close();
+            }
+            catch (SocketException)
+            {
+                // Don't care
+            }
+            catch (ObjectDisposedException)
+            {
+                // Don't care
+            }
+            finally
+            {
+                // Clear this so that the next call to ReceiveMessageBytes() will block
+                // on accepting a new connection.
+                _clientSocket = null;
+            }
+            
+            ConnectionLost?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void Stop()
+        {
+            _tokenSource.Cancel();
+
+            try
+            {
+                _listeningSocket?.Shutdown(SocketShutdown.Both);
+            }
+            catch (SocketException)
+            {
+                // Nop
+            }
+
+            try
+            {
+                _listeningSocket?.Close();
+            }
+            catch (SocketException)
+            {
+                // Nop
+            }
+
+            try
+            {
+                _listeningSocket?.Dispose();
+            }
+            catch
+            {
+                // Nop
+            }
+        }
+
+    }
+
+    public class DataEventArgs : EventArgs
+    {
+        public byte[] Data { get; }
+
+        public DataEventArgs(byte[] data)
+        {
+            Data = data;
+        }
+    }
+}
---
+++ test/RoadCaptain.Adapters.Tests.Unit/Networking/WhenWaitingForIncomingConnection.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace RoadCaptain.Adapters.Tests.Unit.Networking
+{
+    public class WhenWaitingForIncomingConnection
+    {
+        private readonly NetworkConnection _networkConnection;
+        private bool _noConnectionWatchdogRaised;
+        private bool _connectionAcceptedRaised;
+        private readonly int _port;
+        private bool _noDataReceived;
+        private bool _connectionLostRaised;
+        private readonly List<byte> _receivedData = new();
+
+        public WhenWaitingForIncomingConnection()
+        {
+            var random = new Random();
+            _port = random.Next(1025, 10025);
+            _networkConnection = new NetworkConnection(_port, 100, 100);
+            _networkConnection.IncomingConnectionWatchdog += (_, _) => _noConnectionWatchdogRaised = true;
+            _networkConnection.IncomingDataWatchdog += (_, _) => _noDataReceived = true;
+            _networkConnection.Data += (_, args) => _receivedData.AddRange(args.Data);
+            _networkConnection.ConnectionAccepted += (_, _) => _connectionAcceptedRaised = true;
+            _networkConnection.ConnectionLost += (_, _) => _connectionLostRaised = true;
+        }
+
+        [Fact]
+        public void GivenNoConnectionAfterTimeout_NoConnectionEventIsRaised()
+        {
+            var closeTask = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(250);
+                _networkConnection.Stop();
+            });
+
+            try
+            {
+                _networkConnection.StartAsync().GetAwaiter().GetResult();
+            }
+            finally
+            {
+                closeTask.Wait();
+            }
+
+            _noConnectionWatchdogRaised.Should().BeTrue();
+        }
+
+        [Fact]
+        public void GivenConnectionWithinTimeout_ConnectionAcceptedEventIsRaised()
+        {
+            // Setting this up may take some time
+            var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var closeTask = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(250);
+                _networkConnection.Stop();
+            });
+
+            try
+            {
+                var testTask = Task.Factory.StartNew(() =>
+                {
+                    // Need to give the network connection some time to start
+                    // listening to the socket
+                    Thread.Sleep(50);
+
+                    clientSocket.Connect(new IPEndPoint(IPAddress.Loopback, _port));
+
+                    Thread.Sleep(10);
+
+                    clientSocket.Close();
+                    clientSocket.Dispose();
+                });
+
+                var startTask = Task.Factory.StartNew(() => _networkConnection.StartAsync());
+
+                Task.WhenAll(startTask, testTask).GetAwaiter().GetResult();
+            }
+            finally
+            {
+                closeTask.Wait();
+            }
+
+            _connectionAcceptedRaised.Should().BeTrue();
+        }
+
+        [Fact]
+        public void GivenConnectionButNoDataWithinTimeout_NoDataEventIsRaised()
+        {
+            // Setting this up may take some time
+            var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var closeTask = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(250);
+                _networkConnection.Stop();
+            });
+
+            try
+            {
+                var testTask = Task.Factory.StartNew(() =>
+                {
+                    // Need to give the network connection some time to start
+                    // listening to the socket
+                    Thread.Sleep(50);
+
+                    clientSocket.Connect(new IPEndPoint(IPAddress.Loopback, _port));
+
+                    Thread.Sleep(150);
+
+                    clientSocket.Close();
+                    clientSocket.Dispose();
+                });
+
+                var startTask = Task.Factory.StartNew(() => _networkConnection.StartAsync());
+
+                Task.WhenAll(startTask, testTask).GetAwaiter().GetResult();
+            }
+            finally
+            {
+                closeTask.Wait();
+            }
+
+            _noDataReceived.Should().BeTrue();
+        }
+
+        [Fact]
+        public void GivenConnectionAndDataWithinTimeout_DataEventIsRaised()
+        {
+            // Setting this up may take some time
+            var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var closeTask = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(250);
+                _networkConnection.Stop();
+            });
+
+            try
+            {
+                var testTask = Task.Factory.StartNew(() =>
+                {
+                    // Need to give the network connection some time to start
+                    // listening to the socket
+                    Thread.Sleep(50);
+
+                    clientSocket.Connect(new IPEndPoint(IPAddress.Loopback, _port));
+                    
+                    Thread.Sleep(10);
+
+                    clientSocket.Send(new byte[] { 0x1, 0x2, 0x3 });
+                    
+                    Thread.Sleep(10);
+
+                    clientSocket.Close();
+                    clientSocket.Dispose();
+                });
+
+                var startTask = Task.Factory.StartNew(() => _networkConnection.StartAsync());
+
+                Task.WhenAll(startTask, testTask).GetAwaiter().GetResult();
+            }
+            finally
+            {
+                closeTask.Wait();
+            }
+
+            _receivedData
+                .Should()
+                .BeEquivalentTo(new byte[] { 0x1, 0x2, 0x3 });
+        }
+
+        [Fact]
+        public void GivenConnectionAndDataWithinTimeoutInMultipleSends_DataEventIsRaised()
+        {
+            // Setting this up may take some time
+            var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var closeTask = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(250);
+                _networkConnection.Stop();
+            });
+
+            try
+            {
+                var testTask = Task.Factory.StartNew(() =>
+                {
+                    // Need to give the network connection some time to start
+                    // listening to the socket
+                    Thread.Sleep(50);
+
+                    clientSocket.Connect(new IPEndPoint(IPAddress.Loopback, _port));
+                    
+                    Thread.Sleep(10);
+
+                    clientSocket.Send(new byte[] { 0x1, 0x2, 0x3 });
+                    
+                    Thread.Sleep(10);
+
+                    clientSocket.Send(new byte[] { 0x4 });
+                    
+                    Thread.Sleep(10);
+
+                    clientSocket.Send(new byte[] { 0x5, 0x6, 0x7 });
+                    
+                    Thread.Sleep(10);
+
+                    clientSocket.Close();
+                    clientSocket.Dispose();
+                });
+
+                var startTask = Task.Factory.StartNew(() => _networkConnection.StartAsync());
+
+                Task.WhenAll(startTask, testTask).GetAwaiter().GetResult();
+            }
+            finally
+            {
+                closeTask.Wait();
+            }
+
+            _receivedData
+                .Should()
+                .BeEquivalentTo(new byte[] { 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7 });
+        }
+
+        [Fact]
+        public void GivenConnectionAndDataThenClientClosesConnectionReconnectsAndSendsMoreData()
+        {
+            // Setting this up may take some time
+            var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var secondClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var closeTask = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(250);
+                if (!Debugger.IsAttached)
+                {
+                    _networkConnection.Stop();
+                }
+            });
+
+            try
+            {
+                var testTask = Task.Factory.StartNew(() =>
+                {
+                    // Need to give the network connection some time to start
+                    // listening to the socket
+                    Thread.Sleep(50);
+
+                    clientSocket.Connect(new IPEndPoint(IPAddress.Loopback, _port));
+                    
+                    Thread.Sleep(10);
+
+                    var sent = clientSocket.Send(new byte[] { 0x1, 0x2, 0x3 });
+                    
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+
+                    Thread.Sleep(50);
+
+                    secondClientSocket.Connect(new IPEndPoint(IPAddress.Loopback, _port));
+
+                    Thread.Sleep(10);
+
+                    secondClientSocket.Send(new byte[] { 0x4, 0x5, 0x6 });
+                    
+                    secondClientSocket.Shutdown(SocketShutdown.Both);
+                    secondClientSocket.Close();
+
+                    clientSocket.Dispose();
+                    secondClientSocket.Dispose();
+
+                });
+
+                var startTask = Task.Factory.StartNew(() => _networkConnection.StartAsync());
+
+                Task.WhenAll(startTask, testTask).GetAwaiter().GetResult();
+            }
+            finally
+            {
+                closeTask.Wait();
+            }
+
+            _receivedData
+                .Should()
+                .BeEquivalentTo(new byte[] { 0x1, 0x2, 0x3, 0x4, 0x5, 0x6 });
+        }
+
+        [Fact]
+        public void GivenConnectionAndDataThenClientClosesConnection_ConnectionLostEventIsRaised()
+        {
+            // Setting this up may take some time
+            var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var closeTask = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(250);
+                if (!Debugger.IsAttached)
+                {
+                    _networkConnection.Stop();
+                }
+            });
+
+            try
+            {
+                var testTask = Task.Factory.StartNew(() =>
+                {
+                    // Need to give the network connection some time to start
+                    // listening to the socket
+                    Thread.Sleep(50);
+
+                    clientSocket.Connect(new IPEndPoint(IPAddress.Loopback, _port));
+                    
+                    Thread.Sleep(10);
+
+                    var sent = clientSocket.Send(new byte[] { 0x1, 0x2, 0x3 });
+                    
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                    clientSocket.Dispose();
+
+                });
+
+                var startTask = Task.Factory.StartNew(() => _networkConnection.StartAsync());
+
+                Task.WhenAll(startTask, testTask).GetAwaiter().GetResult();
+            }
+            finally
+            {
+                closeTask.Wait();
+            }
+
+            _connectionLostRaised.Should().BeTrue();
+        }
+    }
+}
