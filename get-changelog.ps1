ff827c8dcbf5529afc001cf9afea822a5b028650
Sander van Vliet <sander@codenizer.nl>
2022-10-09 14:15:44 +0200

    Collect changelog for major.minor.patch

--- get-changelog.ps1
+++ get-changelog.ps1
@@ -1,5 +1,11 @@
 param([string]$currentVersion = $(throw "currentVersion is required"))
 
+$parts = $currentVersion.Split(".")
+$currentMajor = $parts[0]
+$currentMinor = $parts[1]
+$currentPatch = $parts[2]
+$currentBuild = $parts[3]
+
 $lines = get-content Changelog.md
 
 $started = $false
@@ -8,12 +14,32 @@ $output = @()
 for($index = 0; $index -lt $lines.Length; $index++)
 {
     $line = $lines[$index].Trim()
-    if($line -eq "## $currentVersion")
+    if($line.StartsWith("## ") -and !$started)
     {
-        $started = $true
+        $parts = $line.Substring(2).Trim().Split(".")
+        $major = $parts[0]
+        $minor = $parts[1]
+        $patch = $parts[2]
+        $build = $parts[3]
+
+        if($major -eq $currentMajor -and $minor -eq $currentMinor -and $patch -eq $currentPatch)
+        {
+            $started = $true
+        }
     }
     elseif($started -and $line.StartsWith("## "))
     {
+        $parts = $line.Substring(2).Trim().Split(".")
+        $major = $parts[0]
+        $minor = $parts[1]
+        $patch = $parts[2]
+        $build = $parts[3]
+
+        if($major -eq $currentMajor -and $minor -eq $currentMinor -and $patch -eq $currentPatch)
+        {
+            continue
+        }
+        
         break
     }
     elseif($started) 
