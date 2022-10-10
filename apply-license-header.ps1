46d60fa200ac52bd64285998f1ba78cff6016ab1
Sander van Vliet <sander@codenizer.nl>
2022-10-10 16:09:42 +0200

    Apply license to XAML files

--- apply-license-header.ps1
+++ apply-license-header.ps1
@@ -17,3 +17,23 @@ foreach($file in $csharpFiles)
         Set-Content $file $targetContent
     }
 }
+
+$xamlFiles = Get-ChildItem -Recurse -Filter *.axaml | where-object {!$_.fullname.Contains("\obj\") -and !$_.fullname.Contains("\bin\")}
+
+$prepend = Get-Content "xaml-license.txt" -Raw
+
+foreach($file in $xamlFiles)
+{
+    $contents = get-content $file -head 2
+    if($contents[1].Trim() -ne "// Copyright (c) 2022 Sander van Vliet")
+    {
+        $fullPath = $file.FullName
+
+        Write-Host "$fullPath doesn't have a license header"
+
+        $rawContents = Get-Content $file -Raw
+
+        $targetContent = $prepend + $rawContents
+        Set-Content $file $targetContent
+    }
+}
