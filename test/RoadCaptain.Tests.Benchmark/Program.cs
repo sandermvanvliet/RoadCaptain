952b59b760fc6410278d0a32c16e00bde99cdf3e
Sander van Vliet <sander@codenizer.nl>
2022-10-11 10:47:26 +0200

    Clean up benchmark project

--- test/RoadCaptain.Tests.Benchmark/Program.cs
+++ test/RoadCaptain.Tests.Benchmark/Program.cs
@@ -2,8 +2,6 @@
 // Licensed under Artistic License 2.0
 // See LICENSE or https://choosealicense.com/licenses/artistic-2.0/
 
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Engines;
 using BenchmarkDotNet.Running;
 
 namespace RoadCaptain.Tests.Benchmark
@@ -13,51 +11,6 @@ namespace RoadCaptain.Tests.Benchmark
         public static void Main(string[] args)
         {
             BenchmarkRunner.Run<TrackPointBenchmarks>();
-        }
-    }
-    
-    [SimpleJob(RunStrategy.Throughput, warmupCount:1)]
-    public class TrackPointBenchmarks
-    {
-        [Params(10000)]
-        public int N;
-
-        [Benchmark(Baseline = true)]
-        public TrackPoint Baseline()
-        {
-            return FromGameLocationBaseline(12, 13, 14, ZwiftWorldId.Watopia);
-        }
-
-        [Benchmark(Baseline = false)]
-        public TrackPoint Inlined()
-        {
-            return new GameCoordinate(12, 13, 14, ZwiftWorldId.Watopia).ToTrackPoint();
-        }
-
-        public static TrackPoint FromGameLocationBaseline(double latitudeOffsetCentimeters, double longitudeOffsetCentimeters,
-            double altitude, ZwiftWorldId worldId)
-        {
-            ZwiftWorldConstants worldConstants;
-            
-            switch (worldId)
-            {
-                case ZwiftWorldId.Watopia:
-                    worldConstants = TrackPoint.ZwiftWorlds[worldId];
-                    break;
-                case ZwiftWorldId.MakuriIslands:
-                    worldConstants = TrackPoint.ZwiftWorlds[worldId];
-                    break;
-                default:
-                    return TrackPoint.Unknown;
-            }
-
-            var latitudeAsCentimetersFromOrigin = latitudeOffsetCentimeters + worldConstants.CenterLatitudeFromOrigin;
-            var latitude = latitudeAsCentimetersFromOrigin / worldConstants.MetersBetweenLatitudeDegree / 100;
-
-            var longitudeAsCentimetersFromOrigin = longitudeOffsetCentimeters + worldConstants.CenterLongitudeFromOrigin;
-            var longitude = longitudeAsCentimetersFromOrigin / worldConstants.MetersBetweenLongitudeDegree / 100;
-
-            return new TrackPoint(latitude, longitude, altitude);
         }
     }
 }
