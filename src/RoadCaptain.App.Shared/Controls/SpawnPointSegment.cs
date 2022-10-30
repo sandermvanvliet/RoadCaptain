cd19a3850ca2414c77ec95a7011fd9a54026d899
Sander van Vliet <sander@codenizer.nl>
2022-10-30 10:43:29 +0100

    Initialize the arrow path when constructing a spawn point segment instead of doing that every render call

--- src/RoadCaptain.App.Shared/Controls/SpawnPointSegment.cs
+++ src/RoadCaptain.App.Shared/Controls/SpawnPointSegment.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Codenizer.Avalonia.Map;
 using SkiaSharp;
 
@@ -6,10 +7,11 @@ namespace RoadCaptain.App.Shared.Controls
     public class SpawnPointSegment : MapObject
     {
         private readonly SKPath _path;
-        private readonly float _angleRadians;
         private readonly SKPaint _arrowPaint;
         private readonly SKPaint _circlePaint;
         private readonly SKPaint _circleFillPaint;
+        private readonly SKPath? _arrowPath;
+        private readonly SKPoint _centerPoint;
         private const int CircleMarkerRadius = 16;
 
         public SpawnPointSegment(string segmentId, SKPoint[] points, double middleBearing)
@@ -21,10 +23,18 @@ namespace RoadCaptain.App.Shared.Controls
             Name = $"spawnPoint-{segmentId}";
             Bounds = _path.TightBounds;
             
-            _angleRadians = (float)TrackPoint.DegreesToRadians(ConvertToSkiaAngle(middleBearing)); 
             _arrowPaint = new SKPaint{ Color = SKColor.Parse("#FFFFFF"), Style = SKPaintStyle.Stroke, StrokeWidth = 6, IsAntialias = true};
             _circlePaint = new() { Color = SKColor.Parse("#FFFFFF"), Style = SKPaintStyle.Stroke, StrokeWidth = 2, IsAntialias = true };
             _circleFillPaint = new() { Color = SKColor.Parse("#0094FF"), Style = SKPaintStyle.Fill, IsAntialias = true };
+            
+            if (points.Any())
+            {
+                _centerPoint = _path.Points[_path.PointCount / 2];
+
+                var angleRadians = (float)TrackPoint.DegreesToRadians(ConvertToSkiaAngle(middleBearing));
+                
+                _arrowPath = CreateArrowPath(angleRadians);
+            }
         }
 
         private static double ConvertToSkiaAngle(double middleBearing)
@@ -46,36 +56,45 @@ namespace RoadCaptain.App.Shared.Controls
             if (IsVisible)
             {
                 canvas.DrawPath(_path, SkiaPaints.SpawnPointSegmentPathPaint);
-                
-                var pathPoint = _path.Points[_path.PointCount/ 2];
-                
-                canvas.DrawCircle(pathPoint, CircleMarkerRadius, _circleFillPaint);
-                canvas.DrawCircle(pathPoint, CircleMarkerRadius, _circlePaint);
 
-                var matrix = SKMatrix.CreateRotation(_angleRadians);
+                canvas.DrawCircle(_centerPoint, CircleMarkerRadius, _circleFillPaint);
+                canvas.DrawCircle(_centerPoint, CircleMarkerRadius, _circlePaint);
 
-                var size = CircleMarkerRadius - 4;
-
-                var chevronPoints = matrix.MapPoints(new[]
-                {
-                    new SKPoint(size, size),
-                    new SKPoint(0, 0),
-                    new SKPoint(size, -size)
-                });
-                var stemPoints = matrix.MapPoints(new[]
-                {
-                    new SKPoint(0, 0),
-                    new SKPoint(2 * size, 0)
-                });
-                
-                var arrowPath = new SKPath();
-                arrowPath.AddPoly(chevronPoints, false);
-                arrowPath.AddPoly(stemPoints, false);
-                
-                arrowPath.Transform(SKMatrix.CreateTranslation(pathPoint.X - arrowPath.TightBounds.MidX, pathPoint.Y - arrowPath.TightBounds.MidY));
-
-                canvas.DrawPath(arrowPath, _arrowPaint);
+                canvas.DrawPath(_arrowPath, _arrowPaint);
             }
+        }
+
+        private SKPath CreateArrowPath(float angleRadians)
+        {
+            const int size = CircleMarkerRadius - 4;
+
+            var matrix = SKMatrix.CreateRotation(angleRadians);
+            
+            var chevronPoints = matrix.MapPoints(new[]
+            {
+                new SKPoint(size, size),
+                new SKPoint(0, 0),
+                new SKPoint(size, -size)
+            });
+
+            var stemPoints = matrix.MapPoints(new[]
+            {
+                new SKPoint(0, 0),
+                new SKPoint(2 * size, 0)
+            });
+
+            var arrowPath = new SKPath();
+
+            arrowPath.AddPoly(chevronPoints, false);
+            arrowPath.AddPoly(stemPoints, false);
+
+            arrowPath
+                .Transform(
+                    SKMatrix.CreateTranslation(
+                        _centerPoint.X - arrowPath.TightBounds.MidX,
+                        _centerPoint.Y - arrowPath.TightBounds.MidY));
+
+            return arrowPath;
         }
     }
 }
