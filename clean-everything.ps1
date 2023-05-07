efae0857e291a8fb300af275691bb7ebf7a0fbf6
Sander van Vliet <sander@codenizer.nl>
2023-05-07 14:45:20 +0200

    More xplat build tweaking

---
+++ clean-everything.ps1
@@ -0,0 +1,5 @@
+# This script is to help with ensuring a clean environment when switching
+# runtime identifiers or target frameworks.
+# Usually you'll run into problems because the package caches get left around
+# and nothing will compile because dotnet build will complain.
+get-childitem -Include bin,obj -Recurse |  Remove-Item -Recurse -Path { $_.FullName }
