ad92dc837d4daa79ba7409380ce9b4a39ff6dd16
Sander van Vliet <sander@codenizer.nl>
2023-07-06 14:08:47 +0200

    Apply license headeres

--- src/RoadCaptain.App.Shared/ViewModels/DesignTimeSelectRouteWindowViewModel.cs
+++ src/RoadCaptain.App.Shared/ViewModels/DesignTimeSelectRouteWindowViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+// Copyright (c) 2023 Sander van Vliet
+// Licensed under Artistic License 2.0
+// See LICENSE or https://choosealicense.com/licenses/artistic-2.0/
+
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using RoadCaptain.Ports;
 using RoadCaptain.UseCases;
