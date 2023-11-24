1d5c029f579c51d945febfc68fba5ba1debbf10d
Sander van Vliet <sander@codenizer.nl>
2023-11-24 12:50:11 +0100

    Add license header

--- src/RoadCaptain.App.Shared/AppSecurityTokenProvider.cs
+++ src/RoadCaptain.App.Shared/AppSecurityTokenProvider.cs
@@ -1,4 +1,8 @@
-﻿using System;
+// Copyright (c) 2023 Sander van Vliet
+// Licensed under Artistic License 2.0
+// See LICENSE or https://choosealicense.com/licenses/artistic-2.0/
+
+using System;
 using System.Threading.Tasks;
 using Microsoft.IdentityModel.JsonWebTokens;
 using RoadCaptain.App.Shared.Models;
