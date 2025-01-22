0fd4866365cfa9368292a1842d91d80d76f8b8bd
Sander van Vliet <sander@codenizer.nl>
2025-01-22 10:27:30 +0100

    Support updating license header

--- apply-license-header.ps1
+++ apply-license-header.ps1
@@ -1,23 +1,39 @@
-# Copyright (c) 2023 Sander van Vliet
+# Copyright (c) 2025 Sander van Vliet
 # Licensed under Artistic License 2.0
 # See LICENSE or https://choosealicense.com/licenses/artistic-2.0/
 $csharpFiles = Get-ChildItem -Recurse -Filter *.cs | where-object {!$_.fullname.Contains("\obj\") -and !$_.fullname.Contains("\bin\")}
 
 $prepend = Get-Content "csharp-license.txt" -Raw
+$licenseFirstLine = (Get-Content "csharp-license.txt" -head 1).Trim()
 
 foreach($file in $csharpFiles)
 {
     $contents = get-content $file -head 1
-    if(!$contents.Trim().StartsWith("// Copyright (c) "))
+    $contentsTrimmed = $contents.Trim()
+    
+    if(!$contentsTrimmed.StartsWith("// Copyright (c) "))
     {
         $fullPath = $file.FullName
 
-        Write-Host "$fullPath doesn't have a license header"
+        Write-Host "$fullPath doesn't have a license header, adding it"
 
         $rawContents = Get-Content $file -Raw
 
         $targetContent = $prepend + $rawContents
-        Set-Content $file $targetContent
+        
+        $targetContent | Out-File $file -NoNewline
+    }
+    elseif($ccontentsTrimmed -ne $licenseFirstLine)
+    {
+        $fullPath = $file.FullName
+
+        Write-Host "$fullPath has a license header but it's not up to date, changing it"
+
+        $rawContents = Get-Content $file -Raw
+        
+        $targetContent = $rawContents.Replace($contents.Trim(), $licenseFirstLine)
+
+        $targetContent | Out-File $file -NoNewline
     }
 }
 
